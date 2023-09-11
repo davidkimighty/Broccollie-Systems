3d4ddb949264f81ac1d50b7c978c03879841b3ca
Hyunwoo Kim <hyunwookimbob@gmail.com>
2023-09-11 15:31:14 +0900

    #2 close - separating notification features

--- Runtime/Notification/NotificationsController.cs
+++ Runtime/Notification/NotificationsController.cs
@@ -1,6 +1,3 @@
-using System;
-using Broccollie.UI;
-using TMPro;
 using UnityEngine;
 
 namespace Broccollie.System
@@ -8,73 +5,30 @@ namespace Broccollie.System
     public class NotificationsController : MonoBehaviour
     {
         [SerializeField] private NotificationsEventChannel _eventChannel;
-
-        [Header("Full Screen")]
-        [SerializeField] private PanelUI _fullScreenPanel;
-        [SerializeField] private TMP_Text _fullScreenMessageText;
-
-        [Header("Popup Confirm")]
-        [SerializeField] private PanelUI _popupConfirmPanel;
-        [SerializeField] private TMP_Text _popupConfirmMessageText;
-        [SerializeField] private ButtonUI _popupConfirmButton;
-
-        private void Awake()
-        {
-            _popupConfirmButton.OnSelect += (eventArgs) => PopupConfirmedAsync();
-        }
+        [SerializeField] private FullScreenNotification _fullScreenNotification;
+        [SerializeField] private PopupConfirmNotification _popupConfirmNotification;
 
         private void OnEnable()
         {
-            _eventChannel.OnFullscreenMessage += ShowFullscreenMessageAsync;
-            _eventChannel.OnPopupConfirmMessage += ShowPopupConfirmMessageAsync;
+            if (_fullScreenNotification != null)
+                _eventChannel.OnFullscreenMessage += ShowFullScreenNotification;
+            if (_popupConfirmNotification != null)
+                _eventChannel.OnPopupConfirmMessage += ShowPopupConfirmNotification;
         }
 
         private void OnDisable()
         {
-            _eventChannel.OnFullscreenMessage -= ShowFullscreenMessageAsync;
-            _eventChannel.OnPopupConfirmMessage -= ShowPopupConfirmMessageAsync;
+            if (_fullScreenNotification != null)
+                _eventChannel.OnFullscreenMessage -= ShowFullScreenNotification;
+            if (_popupConfirmNotification != null)
+                _eventChannel.OnPopupConfirmMessage -= ShowPopupConfirmNotification;
         }
 
         #region Subscribers
-        private async void ShowFullscreenMessageAsync(bool state, string message)
-        {
-            if (_fullScreenMessageText != null)
-                _fullScreenMessageText.text = message;
-            await _fullScreenPanel.SetActiveAsync(state);
-        }
+        private void ShowFullScreenNotification(bool state, string message) => _fullScreenNotification.ShowMessageAsync(state, message);
 
-        private async void ShowPopupConfirmMessageAsync(bool state, string message)
-        {
-            if (_popupConfirmMessageText != null)
-                _popupConfirmMessageText.text = message;
-            await _popupConfirmPanel.SetActiveAsync(state);
-
-        }
-
-        private async void PopupConfirmedAsync()
-        {
-            if (_popupConfirmMessageText != null)
-                _popupConfirmMessageText.text = string.Empty;
-            await _popupConfirmPanel.SetActiveAsync(false);
-        }
+        private void ShowPopupConfirmNotification(bool state, string message) => _popupConfirmNotification.ShowMessageAsync(state, message);
 
         #endregion
-    }
-
-    public class FullScreenMessage : IDisposable
-    {
-        private NotificationsEventChannel _eventChannel;
-
-        public FullScreenMessage(NotificationsEventChannel eventChannel, string message)
-        {
-            _eventChannel = eventChannel;
-            _eventChannel.RequestFullscreenMessage(true, message);
-        }
-
-        public void Dispose()
-        {
-            if (_eventChannel == null) return;
-            _eventChannel.RequestFullscreenMessage(false);
-        }
     }
 }
